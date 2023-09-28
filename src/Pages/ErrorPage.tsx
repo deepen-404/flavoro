41b693c67fe5331ff0626ba8f42d9000f160f172
deepen-404 <141730570+deepen-404@users.noreply.github.com>
2023-09-28 23:22:45 +0545

    updating error page

--- src/Pages/ErrorPage.tsx
+++ src/Pages/ErrorPage.tsx
@@ -1,9 +1,20 @@
-const ErrorPage = () => {
-  return (
-    <div>
-      error
-    </div>
-  )
-}
+import { useNavigate } from "react-router-dom";
 
-export default ErrorPage
+const ErrorPage = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="h-screen flex justify-center  flex-col items-center">
+      <h2 className="font-semibold text-2xl">
+        The page you have requested doesn't exist😢.
+      </h2>
+      <button
+        onClick={() => navigate("/")}
+        className="bg-green-500 px-4 py-3 rounded-3xl mt-7 text-lg font-semibold text-gray-100 hover:bg-gray-400 hover:translate-y-[-10%] transition-all duration-200 ease-in"
+      >
+        Go to Homepage
+      </button>
+    </div>
+  );
+};
+
+export default ErrorPage;
